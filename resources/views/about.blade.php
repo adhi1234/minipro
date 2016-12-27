3b4b09d644ca9fdccd6c34019bdc239abca9686f
Adarsh Sreekumar <astromediasnet16@gmail.com>
2016-12-27 17:00:47 +0530

    Team members added
    We are doing a mini project for college ...so HANG UPP

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,4 +2,17 @@
 @section('content')
 <h1>About Us</h1>
 <p>Learning Laravel is easy for me.Thanks PSN.</p>
+
+
+.<div class="container">
+  .<section class="section-padding">
+    .<div class="jumbotron text-center">
+      .<h1><span class="grey">Welcome to </span>our first intro</h1>
+<p>We will be doing our master project for our college soon.
+  Stay connected..
+    .<li>Dimal</li>
+    .<li>Adarsh</li>
+    .<li>Alvin</li>
+    .<li>Dhanya T</li>
+  <p>
 @stop
