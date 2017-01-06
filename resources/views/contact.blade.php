3fe2262889ef25e51c32b6fcf5aa2c27d791b739
Adarsh Sreekumar <astromediasnet16@gmail.com>
2017-01-06 23:31:26 +0530

    unwanted_jquery
    Removed js files which was made for a test run

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 <h1>Contact Us</h1>
+<section class="header">
+  <div class="background"> &nbsp;</div>
+  </section>
+  .<div class="container">
+    .<section class="section-padding">
+      .<div class="jumbotron text-center">
+        .<h1><span class="grey">Welcome to </span>Project</h1>
+  <p>Wanna Learn to Code .Here is the rigt solution that you had found.<p>
+
+      </div>
 <p>Please contact us by sending a message</p>
 
 
